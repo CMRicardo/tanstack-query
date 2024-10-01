fcf284261cbc317c662f540ee3b99400c0839b79
Ricardo Corrales <rcorrales@grupocomidas.com>
2024-10-01 09:50:18 -0600

    Add github token env variable

--- src/issues/hooks/use-labels.tsx
+++ src/issues/hooks/use-labels.tsx
@@ -33,32 +33,6 @@ export const useLabels = () => {
         default: false,
       } satisfies GitHubLabel,
     ], */
-    initialData: [
-      {
-        id: 791921801,
-        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
-        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
-        name: "❤️",
-        color: "ffffff",
-        default: false,
-      } satisfies GitHubLabel,
-      {
-        id: 588833528,
-        node_id: "MDU6TGFiZWw1ODg4MzM1Mjg=",
-        url: "https://api.github.com/repos/facebook/react/labels/Difficulty:%20medium",
-        name: "Difficulty: medium",
-        color: "fbca04",
-        default: false,
-      } satisfies GitHubLabel,
-      {
-        id: 710400704,
-        node_id: "MDU6TGFiZWw3MTA0MDA3MDQ=",
-        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Test%20Renderer",
-        name: "Component: Test Renderer",
-        color: "006b75",
-        default: false,
-      } satisfies GitHubLabel,
-    ],
   });
 
   return { labelsQuery };
