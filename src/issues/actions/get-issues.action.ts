64039396caf4825db3f8f2b0dece68dacae758ad
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-10-06 11:05:45 -0600

    End of section 6

--- src/issues/actions/get-issues.action.ts
+++ src/issues/actions/get-issues.action.ts
@@ -2,12 +2,19 @@ import { githubApi } from "../../api";
 import { sleep } from "../../helpers";
 import { GitHubIssue, State } from "../interfaces";
 
-export const getIssues = async (state: State): Promise<GitHubIssue[]> => {
+export const getIssues = async (
+  state: State,
+  selectedLabels: string[],
+): Promise<GitHubIssue[]> => {
   await sleep(1500);
   const params = new URLSearchParams();
 
   if (state !== State.All) {
     params.append("state", state);
+  }
+
+  if (selectedLabels.length > 0) {
+    params.append("labels", selectedLabels.join(","));
   }
 
   const { data } = await githubApi.get<GitHubIssue[]>("/issues", { params });
--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,12 @@
 import { LoadingSpinner } from "../../shared";
 import { useLabels } from "../hooks";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+
+  onLabelSelected: (label: string) => void;
+}
+export const LabelPicker = ({ selectedLabels, onLabelSelected }: Props) => {
   const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading) {
@@ -17,7 +22,10 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+          onClick={() => onLabelSelected(label.name)}
+          className={`animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer
+              ${selectedLabels.includes(label.name) ? "selected-label" : ""}
+            `}
           style={{
             border: `1px solid #${label.color}`,
           }}
